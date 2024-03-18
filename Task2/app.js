03d4797eb7dcd592b26fe0820fc952e918bc28df
K0STI@ <ksinogub333@gmail.com>
2024-03-18 21:14:05 +0100

    Task2

--- Task2/app.js
+++ Task2/app.js
@@ -1,23 +1,30 @@
-const express = require('express');
-const app = express();
+const http = require('http');
+const routes = require('./routes/index');
 const PORT = 3000;
 
-app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-    res.render('home', { pageName: 'Home Page' });
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        routes.handleHome(req, res);
+    } else if (req.url === '/student') {
+        routes.handleStudent(req, res);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end(`
+        <html>
+        <head>
+          <title>Error</title>
+        </head>
+        <body>
+          <p>EROR 404</p>
+        </body>
+      </html>
+        `);
+    }
 });
 
-app.get('/student', (req, res) => {
-    res.render('student', { pageName: 'Student Page' });
-});
-app.use((req, res) => {
-    res.status(404).send('404 Not Found');
-});
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 });
 
-app.post('/student', (req, res) => {
-    const { firstName, lastName, age, gender, code, studyField } = req.body;
-    res.send('Form submitted successfully!');
-});
+
